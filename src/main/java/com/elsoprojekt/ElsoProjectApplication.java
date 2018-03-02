9fcf2cf8233c376c6d8d28bc393c7684bb773182
'mrchaos007' <david.facsko@gmail.com>
2018-03-02 12:48:37 +0100

    15 - SFJ - Spring Boot Ismeretek - első lépések

---
+++ src/main/java/com/elsoprojekt/ElsoProjectApplication.java
@@ -0,0 +1,12 @@
+package com.elsoprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElsoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElsoProjectApplication.class, args);
+	}
+}
