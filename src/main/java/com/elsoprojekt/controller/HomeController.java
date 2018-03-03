bd005c6280865e5ff85cda0a8b677174adbe53f7
'mrchaos007' <david.facsko@gmail.com>
2018-03-03 14:21:18 +0100

    21 - SFJ - Kivételkezelés

--- src/main/java/com/elsoprojekt/controller/HomeController.java
+++ src/main/java/com/elsoprojekt/controller/HomeController.java
@@ -4,8 +4,12 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.elsoprojekt.domain.Story;
@@ -19,6 +23,19 @@ public class HomeController {
 		model.addAttribute("stories", getStories());
 		System.out.println(String.format("Request received. Language: %s, Counry: %s %n",  locale.getLanguage(), locale.getDisplayCountry()));
 		return "stories";
+	}
+	
+	@RequestMapping("/user/{id}")
+	public String searchForUser(@PathVariable(value="id") String id) throws Exception {
+		if(id == null)
+			throw new Exception("Nincs ilyen ID-val felhasználónk");
+		return "user";
+	}
+	
+	@ExceptionHandler(Exception.class)
+	public String exceptionHandler(HttpServletRequest ra, Exception ex, Model model) {
+		model.addAttribute("errorMessage", ex.getMessage());
+		return "exceptionHandler";
 	}
 	
 	private ArrayList<Story> getStories() {
